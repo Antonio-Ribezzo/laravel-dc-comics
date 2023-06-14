a990475aa3f44d7ae15765c0a04695d0217c42ae
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-14 16:30:15 +0200

    Seeder (popolo tab. comics con i dati nel config)

--- database/migrations/2023_06_14_135438_create_comics_table.php
+++ database/migrations/2023_06_14_135438_create_comics_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->text('description');
-            $table->string('thumb');
+            $table->text('thumb');
             $table->string('price', 20);
             $table->string('series');
             $table->date('sale_date');
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -14,6 +16,21 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //porto i dati del config
+        $datiComics = config('comics');
+
+        foreach( $datiComics as $el){
+            $newComic = new Comic();
+
+            $newComic->title = $el['title'];
+            $newComic->description = $el['description'];
+            $newComic->thumb = $el['thumb'];
+            $newComic->price = $el['price'];
+            $newComic->series = $el['series'];
+            $newComic->sale_date = $el['sale_date'];
+            $newComic->type = $el['type'];
+
+            $newComic->save();
+        }
     }
 }
