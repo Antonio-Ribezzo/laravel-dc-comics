6f8b3aa4c1ffbe91c1a910a7ea5a6aded77aeecb
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-14 16:56:57 +0200

    Structure Review

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,43 @@
         <!-- Contenuto variabile in base alle pagine -->
         @yield( 'contentHome' )
 
+        <section id="sectionStore">
+            <div class="container py-5">
+                <ul class="d-flex justify-content-around align-items-center">
+                    <li>
+                        <a href="#" class="d-flex justify-content-center    align-items-center">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="icon1">
+                            <span class="ms-3 text-uppercase">digital comics</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="d-flex justify-content-center    align-items-center">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="icon2">
+                            <span class="ms-3 text-uppercase">dc merchandise</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="d-flex justify-content-center    align-items-center">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="icon3">
+                            <span class="ms-3 text-uppercase">subscription</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="d-flex justify-content-center    align-items-center">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="icon4">
+                            <span class="ms-3 text-uppercase">comic shop locator</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="d-flex justify-content-center    align-items-center">
+                            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="icon5">
+                            <span class="ms-3 text-uppercase">dcpowervisa</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
     </main>
 
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title') 
-    DC Comics | Home
+    DC Comics | All Comics
 @endsection
 
 
@@ -26,51 +26,5 @@
             </ul>
             <button class="text-white text-uppercase">load more</button>
         </div>
-
-
     </section>
-
-
-
-
-
-
-    <section id="sectionStore">
-        <div class="container py-5">
-            <ul class="d-flex justify-content-around align-items-center">
-                <li>
-                    <a href="#" class="d-flex justify-content-center align-items-center">
-                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="icon1">
-                        <span class="ms-3 text-uppercase">digital comics</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-center align-items-center">
-                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="icon2">
-                        <span class="ms-3 text-uppercase">dc merchandise</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-center align-items-center">
-                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="icon3">
-                        <span class="ms-3 text-uppercase">subscription</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-center align-items-center">
-                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="icon4">
-                        <span class="ms-3 text-uppercase">comic shop locator</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-center align-items-center">
-                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="icon5">
-                        <span class="ms-3 text-uppercase">dcpowervisa</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </section>
-
-    
 @endsection
